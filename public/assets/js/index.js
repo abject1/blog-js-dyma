fdf826cca923c98764a86b3ad27d771f65caff78
lucas wurtz <lucaswurtz0@gmail.com>
2022-11-06 17:20:36 +0100

    fin de la première partie

---
+++ public/assets/js/index.js
@@ -0,0 +1,49 @@
+const articleContainerElement = document.querySelector('.articles-container');
+const createArticles = articles => {
+  const articlesDOM = articles.map(article => {
+    const articleDOM = document.createElement('div');
+    articleDOM.classList.add('article');
+    articlesDOM.innerHTML = `
+        <img src="${article.img}" alt="profile">
+        <h2>${article.title}</h2>
+        <p class="article-author">${article.author} - ${article.category}</p>
+        <p class="article-content">
+            ${article.content}
+        </p>
+        <div class="article-action">
+            <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
+        </div>
+        `;
+    return articleDOM;
+  });
+  articleContainerElement.innerHTML = '';
+  articleContainerElement.append(...articlesDOM);
+  const deleteButtons = articleContainerElement.querySelectorAll('.btn-danger');
+  deleteButtons.forEach(button => {
+    button.addEventListener('click', async event => {
+      try {
+        const target = event.target;
+        const articleId = target.dataset.id;
+        const response = fetch(`https://restapi.fr/api/article-abject/${articleId}`, {
+          method: "DELETE"
+        });
+        const body = await response.json();
+        console.log(body);
+        fetchArticle();
+      } catch (e) {
+        console.log("e : ", e);
+      }
+    });
+  });
+};
+const fetchArticle = async () => {
+  try {
+    const response = await fetch("https://restapi.fr/api/article-abject");
+    const articles = await response.json();
+    console.log(articles);
+    createArticles(articles);
+  } catch (e) {
+    console.log("e : ", e);
+  }
+};
+fetchArticle();
